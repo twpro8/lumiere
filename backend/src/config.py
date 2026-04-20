e1f3eb7a4878bfd938b8f8acda0f8c0b9459f5e3
John Davis <twpro8@gmail.com>
2026-04-20 13:39:28 +0300

    feat: create config.py with pydantic settings

---
+++ backend/src/config.py
@@ -0,0 +1,31 @@
+from pydantic import computed_field, PostgresDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_ignore_empty=True,
+        extra="ignore",
+    )
+
+    POSTGRES_HOST: str = "localhost"
+    POSTGRES_PORT: int = 5432
+    POSTGRES_USER: str
+    POSTGRES_PASS: str
+    POSTGRES_DB: str
+
+    @computed_field  # type: ignore
+    @property
+    def DATABASE_URL(self) -> PostgresDsn:
+        return PostgresDsn.build(
+            scheme="postgresql+asyncpg",
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASS,
+            host=self.POSTGRES_HOST,
+            port=self.POSTGRES_PORT,
+            path=self.POSTGRES_DB,
+        )
+
+
+settings = Settings()
