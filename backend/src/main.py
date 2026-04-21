81975fdac71d340a7a3dc693ed18173d12a11d99
John Davis <twpro8@gmail.com>
2026-04-21 14:52:53 +0300

    feat: add docker-compose.yaml add global and local backend env files. add env examples

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 
 from src.user import router as user_router
+from src.config import settings
 
-app = FastAPI()
+app = FastAPI(title=settings.APP_NAME)
 app.include_router(user_router)
 
 
