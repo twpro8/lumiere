957393dc18a09a369762ba11b34d72526a1b2d96
kireev.d <kireev.d69@yandex.ru>
2026-04-22 22:47:48 +0300

    docs: removed unnecessary description

--- backend/src/user/router.py
+++ backend/src/user/router.py
@@ -7,12 +7,7 @@ router = APIRouter(prefix="/users", tags=["Users"])
 
 @router.post("/register", summary='Register new user')
 async def register_user(user_data: UserCreateSchema, session: SessionDep) -> dict[str, str]:
-    """
-    Add new user
-    :param user_data: data used to create user
-    :param session: Session used to create user
-    :return: dict[str, str]
-    """
+    """Add new user"""
     await UserRepository(session).create(user_data)
     await session.commit()
     return {'detail': 'User successfully created!'}
