853feaaa267d0bcae08b2f22e61b1c302a405f6e
kireev.d <kireev.d69@yandex.ru>
2026-04-22 22:38:19 +0300

    refactoring router for user

--- backend/src/user/router.py
+++ backend/src/user/router.py
@@ -6,8 +6,13 @@ from backend.src.user.repository import UserRepository
 router = APIRouter(prefix="/users", tags=["Users"])
 
 @router.post("/register", summary='Register new user')
-async def register_user(data: UserCreateSchema, session: SessionDep):
-    """Add new user"""
-    await UserRepository(session).create(data)
+async def register_user(user_data: UserCreateSchema, session: SessionDep) -> dict[str, str]:
+    """
+    Add new user
+    :param user_data: data used to create user
+    :param session: Session used to create user
+    :return: dict[str, str]
+    """
+    await UserRepository(session).create(user_data)
     await session.commit()
     return {'detail': 'User successfully created!'}
