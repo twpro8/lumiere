4e34de051b459a3a1f443fcbfdb3eb4b70eeefcd
kireev.d <kireev.d69@yandex.ru>
2026-04-22 22:35:46 +0300

    feat: add router for register user

--- backend/src/user/router.py
+++ backend/src/user/router.py
@@ -1,8 +1,13 @@
 from fastapi import APIRouter
+from backend.src.core.dependencies import SessionDep
+from backend.src.user.schemas import UserCreateSchema
+from backend.src.user.repository import UserRepository
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
-
-@router.get("/")
-async def get_users() -> dict[str, str]:
-    return {"message": "List of users"}
+@router.post("/register", summary='Register new user')
+async def register_user(data: UserCreateSchema, session: SessionDep):
+    """Add new user"""
+    await UserRepository(session).create(data)
+    await session.commit()
+    return {'detail': 'User successfully created!'}
