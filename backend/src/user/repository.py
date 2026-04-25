8a62d05a7c811ac265599ed433b49e0ed2fc8a59
kireev.d <kireev.d69@yandex.ru>
2026-04-25 23:13:07 +0300

    fix: removed the unnecessary

--- backend/src/user/repository.py
+++ backend/src/user/repository.py
@@ -1,22 +1,8 @@
-from backend.src.core.base_repository import BaseRepository
-from backend.src.user.model import UserOrm
-from backend.src.user.schemas import UserSchema
-from pydantic import BaseModel
-from sqlalchemy import insert
-from backend.src.user.security import hash_password
+from src.core.repositories import BaseRepository
+from src.user.model import UserOrm
+from src.user.schemas import UserSchema
 
 
 class UserRepository(BaseRepository[UserOrm, UserSchema]):
     model = UserOrm
     schema = UserSchema
-
-    async def create(self, data_user: BaseModel) -> None:
-        """
-        Create user
-        :param data_user: data for user creation
-        :return: None
-        """
-        user_d = data_user.model_dump()
-        user_d['password_hash'] = hash_password(user_d.pop('password'))
-        statement = insert(self.model).values(**user_d).returning(self.model)
-        await self.session.execute(statement)
