91e30e61c689fa0bd23f615961e54c73be5e052d
kireev.d <kireev.d69@yandex.ru>
2026-04-25 23:13:58 +0300

    feat: adding new login and me handles, as well as fixing the old register handle

--- backend/src/user/router.py
+++ backend/src/user/router.py
@@ -1,13 +1,34 @@
-from fastapi import APIRouter
-from backend.src.core.dependencies import SessionDep
-from backend.src.user.schemas import UserCreateSchema
-from backend.src.user.repository import UserRepository
+from fastapi import APIRouter, Response
+
+from src.core.dependencies import SessionDep
+from src.user.schemas import UserCreateRequestSchema, UserLoginSchema
+from src.user.service import UserService
+from src.user.dependencies import UserIdDep
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 @router.post("/register", summary='Register new user')
-async def register_user(user_data: UserCreateSchema, session: SessionDep) -> dict[str, str]:
+async def register_user(
+        user_data: UserCreateRequestSchema,
+        session: SessionDep
+) -> dict[str, str]:
     """Register new user"""
-    await UserRepository(session).create(user_data)
-    await session.commit()
+    await UserService(session).create_user(user_data)
     return {'detail': 'User successfully created!'}
+
+
+@router.post("/login", summary='Login user')
+async def login_user(
+        data_login: UserLoginSchema,
+        session: SessionDep,
+        response: Response,
+):
+    """Login user"""
+    await UserService(session).authenticate_user(data_login, response)
+    return {'detail': 'User successfully logged in!'}
+
+
+@router.get("/me", summary='Get current user')
+async def current_user(user_id: UserIdDep, session: SessionDep):
+    """Get current user"""
+    return await UserService(session).get_current_user(user_id)
