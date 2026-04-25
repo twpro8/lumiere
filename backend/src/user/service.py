e00669581f532aa43a859ad24c32b28efd9b089f
kireev.d <kireev.d69@yandex.ru>
2026-04-25 23:14:31 +0300

    feat: added a service layer for the user

---
+++ backend/src/user/service.py
@@ -0,0 +1,56 @@
+from uuid import UUID
+
+from fastapi import HTTPException, Response
+from sqlalchemy.exc import IntegrityError
+
+from src.core.services import BaseService
+from src.user.schemas import UserCreateRequestSchema, UserCreateSchema, UserLoginSchema
+from src.auth.security import hash_password, create_access_token, verify_password, decode_token
+from src.user.repository import UserRepository
+
+
+class UserService(BaseService):
+
+    async def create_user(self, user_data: UserCreateRequestSchema):
+        """
+        Create a new user
+        :param user_data: - user data
+        """
+        try:
+            user_data_add = UserCreateSchema(
+                **user_data.model_dump(),
+                password_hash=hash_password(user_data.password)
+            )
+            await UserRepository(self.session).create(user_data_add)
+            await self.session.commit()
+        except IntegrityError:
+            raise HTTPException(status_code=409, detail="User already exists!")
+
+
+    async def authenticate_user(self, user_data: UserLoginSchema, response: Response):
+        """
+        Authenticate a user
+        :param user_data: - user data
+        :param response: - user data
+        """
+        user = await UserRepository(self.session).get_one(username=user_data.username)
+        if not user:
+            raise HTTPException(status_code=401, detail="Incorrect username or password!")
+
+        if not verify_password(user_data.password, user.password_hash):
+            raise HTTPException(status_code=401, detail="Incorrect username or password!")
+
+        access_token = create_access_token({'user_id':str(user.id)})
+        response.set_cookie('access_token', access_token)
+
+
+    async def get_current_user(self, user_id: UUID):
+        """
+        Get current user
+        :param user_id: - user id
+        :return: - user
+        """
+        user = await UserRepository(self.session).get_one(id=user_id)
+        if not user:
+            raise HTTPException(status_code=404, detail="User not found!")
+        return user
