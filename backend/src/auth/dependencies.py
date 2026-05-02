6560d19b64027ff82411e7c57a3d9646ce14614d
kireev.d <kireev.d69@yandex.ru>
2026-05-02 15:48:44 +0300

    feat(auth): add create refresh token logic #28

--- backend/src/auth/dependencies.py
+++ backend/src/auth/dependencies.py
@@ -3,15 +3,16 @@ from typing import Annotated
 from fastapi import Depends
 
 from src.auth.service import AuthService
-from src.core.dependencies import SessionDep, UserRepositoryDep
+from src.core.dependencies import SessionDep, UserRepositoryDep, RedisDep
 
 
 def get_auth_service(
     session: SessionDep,
     user_repository: UserRepositoryDep,
+    client_redis: RedisDep,
 ) -> AuthService:
     """get auth service"""
-    return AuthService(session, user_repository)
+    return AuthService(session, user_repository, client_redis)
 
 
 AuthServiceDep = Annotated[AuthService, Depends(get_auth_service)]
