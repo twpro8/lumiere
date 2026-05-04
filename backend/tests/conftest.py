35077e307e0661a3736c7182aaa004a92bf6631a
John <twpro8@gmail.com>
2026-05-04 21:49:44 +0300

    Revert ""chore(chat): run black, some different changes""
    This reverts commit 8b7a4c4ebc31957b16eee9b06d0af01ab8930bb0.

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -13,14 +13,14 @@ from src.core.models import *  # noqa
 from tests.dependency_overrides.session import get_null_pool_session
 from tests.seeder import populate_database
 
-data_for_registration = {
+data_for_register = {
     "name": "string",
     "username": "string",
     "email": "user@example.com",
     "password": "string",
 }
 
-data_for_authorization = {"username": "string", "password": "string"}
+data_for_login = {"username": "string", "password": "string"}
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -63,8 +63,8 @@ async def async_client() -> AsyncGenerator[AsyncClient, Any]:
 
 @pytest.fixture(name="ac_auth")
 async def ac_auth(ac: AsyncClient) -> AsyncClient:
-    await ac.post("/auth/register", json=data_for_registration)
-    response = await ac.post("/auth/login", json=data_for_authorization)
+    await ac.post("/auth/register", json=data_for_register)
+    response = await ac.post("/auth/login", json=data_for_login)
 
     assert response.status_code == 200
     return ac
