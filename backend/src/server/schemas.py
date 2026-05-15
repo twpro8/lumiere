ca6ad68a4d42728f266fd43bd703764775f55e2e
AlexgDevs <asmetanin014@gmail.com>
2026-05-15 15:12:06 +0500

    fix(server): add init file in server_member module and delete icon_url field in ServerCreateRequestSchema

--- backend/src/server/schemas.py
+++ backend/src/server/schemas.py
@@ -19,7 +19,6 @@ class ServerSchema(BaseSchema):
 class ServerCreateRequestSchema(BaseSchema):
     name: str = Field(..., min_length=1, max_length=100)
     description: str | None = Field(None, max_length=300)
-    icon_url: str | None
 
 
 class ServerCreateSchema(ServerCreateRequestSchema):
