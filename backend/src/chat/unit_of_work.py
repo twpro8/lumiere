b4ed7534055e5b748a7838db26b82b7656f210d0
Alexander <Alexander>
2026-05-20 21:19:35 +0300

    feat(chat): add member repository to unit of work

--- backend/src/chat/unit_of_work.py
+++ backend/src/chat/unit_of_work.py
@@ -1,12 +1,18 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.core.unit_of_work import BaseUnitOfWork
-from src.chat.repositories import ChatRepository
+from src.chat.repositories import ChatRepository, MemberRepository
 
 
 class ChatUnitOfWork(BaseUnitOfWork):
-    def __init__(self, session: AsyncSession, chat_repository: ChatRepository):
+    def __init__(
+        self,
+        session: AsyncSession,
+        chat_repository: ChatRepository,
+        chat_member_repository: MemberRepository,
+    ):
         super().__init__(session)
         self.chats = chat_repository
+        self.members = chat_member_repository
 
     def _uow_marker(self) -> None: ...
