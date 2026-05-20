d290a046feee3fa8929a8856ecfe0353eebea791
Alexander <Alexander>
2026-05-20 21:33:45 +0300

    feat(chat): add chat service and methods for creation chat #66

---
+++ backend/src/chat/service.py
@@ -0,0 +1,97 @@
+from uuid import UUID
+
+from src.chat.enums import ChatMemberRole, ChatType
+from src.chat.schemas import (
+    ChatCreateRequestSchema,
+    ChatCreateSchema,
+    ChatSchema,
+    MemberCreateSchema,
+)
+from src.core.services.base_service import BaseService
+from src.chat.unit_of_work import ChatUnitOfWork
+
+
+class ChatService(BaseService):
+
+    def __init__(self, uow: ChatUnitOfWork):
+        self.uow = uow
+
+    async def create_chat(
+        self, creator_id: UUID, data: ChatCreateRequestSchema
+    ) -> ChatSchema:
+        if data.type == ChatType.private:
+            return await self._get_or_create_private_chat(creator_id, data)
+        else:
+            return await self._create_group_chat(creator_id, data)
+
+    async def _get_or_create_private_chat(
+        self, creator_id: UUID, data: ChatCreateRequestSchema
+    ) -> ChatSchema:
+        if creator_id == data.target_user_id:
+            # todo: raise error
+            raise ValueError
+
+        assert data.target_user_id
+
+        # get chat between
+        existing_chat = await self.uow.chats.find_private_chat(
+            user_a=creator_id,
+            user_b=data.target_user_id,
+        )
+        if existing_chat:
+            return existing_chat
+
+        chat = await self.uow.chats.create(ChatCreateSchema(type=ChatType.private))
+
+        members = [
+            MemberCreateSchema(
+                user_id=creator_id,
+                chat_id=chat.id,
+            ),
+            MemberCreateSchema(
+                user_id=data.target_user_id,
+                chat_id=chat.id,
+            ),
+        ]
+
+        await self.uow.members.add_members(members)
+        await self.uow.commit()
+
+        return chat
+
+    async def _create_group_chat(
+        self, creator_id: UUID, data: ChatCreateRequestSchema
+    ) -> ChatSchema:
+        chat = await self.uow.chats.create(
+            ChatCreateSchema(
+                owner_id=creator_id,
+                type=ChatType.group,
+                name=data.name,
+                description=data.description,
+            )
+        )
+
+        members = []
+
+        if data.member_ids:
+            members = [
+                MemberCreateSchema(
+                    user_id=user_id,
+                    chat_id=chat.id,
+                    role=ChatMemberRole.member,
+                )
+                for user_id in data.member_ids
+            ]
+
+        members.append(
+            MemberCreateSchema(
+                user_id=creator_id,
+                chat_id=chat.id,
+                role=ChatMemberRole.owner,
+            )
+        )
+
+        await self.uow.members.add_members(members)
+        await self.uow.commit()
+
+        return chat
