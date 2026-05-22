1dc762e9fd62724eeab8dcf0128438c8ccf2ce53
John Davis <twpro8@gmail.com>
2026-05-22 18:23:38 +0300

    test(user): add test_get_me #0

---
+++ backend/tests/integration/user/test_get_me.py
@@ -0,0 +1,20 @@
+from httpx import AsyncClient
+
+from src.user.schemas import UserSchema
+
+
+async def test_get_me(authed_client: AsyncClient, current_user: UserSchema) -> None:
+    response = await authed_client.get("/users/me")
+    assert response.status_code == 200
+
+    user = response.json()
+    _current_user = current_user.model_dump(mode="json")
+
+    assert user.get("id") == _current_user["id"]
+    assert user.get("name") == _current_user["name"]
+    assert user.get("username") == _current_user["username"]
+    assert user.get("email") == _current_user["email"]
+    assert user.get("avatar_url") == _current_user["avatar_url"]
+    assert user.get("is_active") == _current_user["is_active"]
+    assert user.get("created_at") == _current_user["created_at"]
+    assert user.get("updated_at") == _current_user["updated_at"]
