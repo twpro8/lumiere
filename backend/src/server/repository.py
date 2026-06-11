d823d9f9a093a0bbaee0f0326a3ec64499552a4e
AlexgDevs <asmetanin014@gmail.com>
2026-06-11 21:58:51 +0500

    feat(server): Implement decrement member count #113

--- backend/src/server/repository.py
+++ backend/src/server/repository.py
@@ -1,3 +1,6 @@
+from uuid import UUID
+
+from sqlalchemy import update
 from src.core.repositories import BaseRepository
 from src.server.mappers import ServerMapper
 from src.server.models import ServerOrm
@@ -8,3 +11,12 @@ class ServerRepository(BaseRepository[ServerOrm, ServerSchema]):
     model = ServerOrm
     schema = ServerSchema
     mapper = ServerMapper
+
+    async def decrement_member_count(self, server_id: UUID) -> ServerSchema:
+        statement = (
+            update(ServerOrm)
+            .where(ServerOrm.id == server_id)
+            .values(member_count=ServerOrm.member_count - 1)
+            .returning(ServerOrm)
+        )
+        return await self._execute_and_map_one(statement)
