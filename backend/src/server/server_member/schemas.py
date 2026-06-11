a640168c7eddd11545429db89cfee38ae154e989
AlexgDevs <asmetanin014@gmail.com>
2026-06-11 21:59:43 +0500

    feat(server): Implement ServerMemberUpdateSchema #113

--- backend/src/server/server_member/schemas.py
+++ backend/src/server/server_member/schemas.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from uuid import UUID
 
 from src.core.schemas.base_schema import BaseSchema
@@ -5,6 +6,7 @@ from src.server.enums import ServerMemberRole
 
 
 class ServerMemberSchema(BaseSchema):
+    id: UUID
     server_id: UUID
     user_id: UUID
     role: ServerMemberRole
@@ -14,3 +16,7 @@ class ServerMemberCreateSchema(BaseSchema):
     server_id: UUID
     user_id: UUID
     role: ServerMemberRole = ServerMemberRole.member
+
+
+class ServerMemberUpdateSchema(BaseSchema):
+    left_at: datetime | None = None
